6826575cee659635072007572c65c346299dd46f
Addhyan01 <addhyankumar@gmail.com>
2025-03-17 16:11:02 +0530

    Login register is created it is dynamic means login and register is in one component dynamic it work

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom' 
+import Login from './pages/Login/Login'
+import ProfileUpdate from './pages/ProfileUpdate/ProfileUpdate'
+import Chat from './pages/Chat/Chat'
 
 const App = () => {
   return (
-    <div>App</div>
+    <Routes>
+      <Route path='/' element={<Login />} />
+      <Route path='/chat' element={<Chat />} />
+      <Route path='/profil' element={<ProfileUpdate />} />
+      
+
+    </Routes>
   )
 }
 
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Login.css"
+import assets from "../../assets/assets"
 
 const Login = () => {
+
+    const [currentState, setCurrentState] = useState('Sign Up');
+    
+
   return (
-    <div>Login</div>
+    <div className='login'> 
+
+    <img src={assets.logo_big} alt="" className="logo" />
+    <form className="login-form">
+      <h2>{currentState}</h2>
+      {currentState === "Sign Up" ? <input type="text" placeholder='username' className="form-input" required /> : null}
+      <input type="email" placeholder='email' className="form-input" required  />
+      <input type="password" placeholder='password' className="form-input" required />
+      <button type='submit' >{currentState === "Sign Up" ? "Create Account" : "Login Now" }</button>
+
+      {
+        currentState === "Sign Up"?<div className="login-term">
+        <input type='checkbox' required />
+        <p>I agree to the terms & conditions</p>
+      </div> : null 
+      }
+      
+      <div className="login-forgot">
+        {
+          currentState === "Sign Up"?  <p className="login-toggle"> Already have an account <span onClick={()=> setCurrentState("login")}> Login Here</span></p>
+          : <p className="login-toggle"> Create an account <span onClick={()=> setCurrentState("Sign Up")}> Click Here</span></p>
+        }
+       
+       
+      </div>
+    </form>
+    
+    
+    </div>
   )
 }
 
